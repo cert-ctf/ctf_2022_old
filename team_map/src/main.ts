517e6c1be32ed2472390f4094715f050e9c9a107
cert-ctf <91723918+cert-ctf@users.noreply.github.com>
2022-10-27 17:34:57 +0200

    Add files via upload

--- team_map/src/main.ts
+++ team_map/src/main.ts
@@ -1,17 +1,17 @@
-/// <reference types="@workadventure/iframe-api-typings/iframe_api" />
+/// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+let currentPopup: any = undefined;
 
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
 
-async function extendedFeatures() {
-    try {
-        await bootstrapExtra()
-        console.log('Scripting API Extra loaded successfully');
-		
-		WA.room.area.onEnter('ApothekeRoom').subscribe(() => {
+	WA.room.area.onEnter('ApothekeRoom').subscribe(() => {
 			WA.room.hideLayer("Apotheke_Roof");
 		})  
 		
@@ -19,9 +19,21 @@ async function extendedFeatures() {
 			WA.room.showLayer("Apotheke_Roof");
 		})  
 
-    } catch (error) {
-        console.error('Scripting API Extra ERROR',error);
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
     }
 }
-extendedFeatures();
+
+export {};
+
+
 
